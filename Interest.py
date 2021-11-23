69970609d906b33aa23e7c4442cc9f3d91ed3a65
Andopio <89560889+Andopio@users.noreply.github.com>
2021-11-22 17:23:11 -0800

    Add files via upload

---
+++ Interest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 22 17:10:22 2021
+
+@author: andop
+"""
+
+principle=float(input("Amount:"))
+time=int(input("Time:"))
+rate=float(input("Rate:"))
+Simple_interest=(principle*time*rate)/100
+print("Interest is:",Simple_interest)
